02106a094d3de39c14abc8bb31e716d7168463f3
siggigauti <siggigauti@gmail.com>
2016-10-31 16:55:54 +0000

    Revert "User og Booking entities"
    This reverts commit ca900d3d10790cc64048423ef5316f8b7a740f1c.

--- src/main/java/com/HBV501/touristEvents/model/Booking.java
+++ src/main/java/com/HBV501/touristEvents/model/Booking.java
@@ -1,56 +1,10 @@
 package com.HBV501.touristEvents.model;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import javax.persistence.Entity;
 
 /**
  * Created by Siggigauti on 31/10/2016.
  */
 @Entity
 public class Booking {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @NotNull
-    private int capacity;
-
-    @ManyToOne
-    private Event event;
-
-    private User user;
-
-    public Booking(){}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public Event getEvent() {
-        return event;
-    }
-
-    public void setEvent(Event event) {
-        this.event = event;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
 }
