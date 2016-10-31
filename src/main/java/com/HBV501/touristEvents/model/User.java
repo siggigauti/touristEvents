4e6d0b2fd2f103a097d9005aa74da2aaa4b63b5e
siggigauti <siggigauti@gmail.com>
2016-10-31 16:59:24 +0000

    Booking og User Entities
    Bætti við attributes í entities og setti annotations fyrir DB. Held að
    þetta sé rétt.

--- src/main/java/com/HBV501/touristEvents/model/User.java
+++ src/main/java/com/HBV501/touristEvents/model/User.java
@@ -1,10 +1,83 @@
 package com.HBV501.touristEvents.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Siggigauti on 31/10/2016.
  */
 @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+    
+    //@Pattern(regexp = "[0-9a-zA-Z]*@[0-9a-zA-Z]*.[a-zA-Z]*")
+
+    @NotNull
+    private String email;
+
+    private String country;
+
+    private boolean isCompany;
+
+    public User(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public boolean isCompany() {
+        return isCompany;
+    }
+
+    public void setCompany(boolean company) {
+        isCompany = company;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    @OneToMany(mappedBy = "user")
+    private List<Booking> bookings = new ArrayList<>();
+
 }
